cfae58d52f5fcb5615e72dd397177ef4731417e3
Isaacoun100 <Isaacoun100@estudiantec.cr>
2021-08-24 21:47:39 -0600

    Logic fixed abd write method
    The logic was optimized and the write method is now fully functional

--- main.cpp
+++ main.cpp
@@ -32,19 +32,34 @@ string readFile(string location){
     ifstream path(location);
 
     if(path.fail()){
-        cout << "File was not found" << endl;
+        cout << "Unable to locate file to read " << endl;
         return text;
     }
 
     while(!path.eof()){
         getline(path,text);
-        cout << "File was found" << endl;
+        cout << "File was read succesfully" << endl;
     }
 
+    path.close();
     return text;
 }
 
-int arrayLength(string numbers){
+void writeFile(string location, string result){
+
+    ofstream path(location);
+
+    if(path.fail()){
+        cout << "Unable to locate file to write" << endl;
+        return;
+    }
+
+    path<<result;
+    path.close();
+
+}
+
+int stringArrayLength(string numbers){
     int result=0;
     for(int i = 0; numbers[i] != '\0' ; i++){
         if (numbers[i]==','){
@@ -62,7 +77,7 @@ void printArray(int arr[], int n)
     cout << endl;
 }
 
-void makeArray(string stringList, int stringArray[], int stringListLenght){
+void makeArray(string stringList, int stringArray[]){
 
     string box;
     for(int i=0, j=0; stringList.length()>=i;i++){
@@ -77,25 +92,50 @@ void makeArray(string stringList, int stringArray[], int stringListLenght){
             j++;
         }
         else{
-            box=box + stringList[i];
+            box+=stringList[i];
         }
     }
 }
 
+int stringLength(string info){
+    int x=0;
+    while(info[x]!='\0'){x++;}
+    return x;
+}
+
+string makeString(string result, int stringArray[], int arraySize){
+
+    for(int i=0; arraySize>=i; i++){
+        result+= to_string(stringArray[i]);
+        if(arraySize!=i){
+            result+=",";
+        }
+    }
+    return result;
+}
+
 int main()
 {
-    string text, path;
-    cout << "Please enter the file path";
-    cin>>path;
+    string text, readpath, writepath,result;
+    cout << "Please enter the file to read";
+    cin >> readpath;
 
-    text=readFile(path);
+    cout << "Please enter the file to write";
+    cin >> writepath;
 
-    int stringListLength = arrayLength(text);
+    text=readFile(readpath);
+
+    int stringListLength = stringArrayLength(text);
     int stringArray[ stringListLength ];
 
-    makeArray(text, stringArray, stringListLength);
+    makeArray(text, stringArray);
     insertionSort(stringArray, stringListLength+1);
 
-    printArray(stringArray, stringListLength+1 );
+    result=makeString(result,stringArray,stringListLength );
+
+    cout<<result<<endl;
+
+    writeFile(writepath,result);
+
     return 0;
 }
